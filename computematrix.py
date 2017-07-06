9713dc744212ff4581c61091d70f0519f99023a0
jeremy0jiang <jeremy0jiang@gmail.com>
2017-07-06 15:04:03 -0700

    add trees

---
+++ computematrix.py
@@ -0,0 +1,56 @@
+import dendropy
+import numpy as np
+import sys, getopt
+sys.setrecursionlimit(1000000000)
+
+if __name__ == "__main__":
+    argv=sys.argv[1:]
+    inputfile = ''
+    outputfile = ''
+    try:
+        opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+    except getopt.GetoptError:
+        print 'test.py -i <inputfile> -o <outputfile>'
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print 'test.py -i <inputfile> -o <outputfile>'
+            sys.exit()
+        elif opt in ("-i", "--ifile"):
+            inputfile = arg
+        elif opt in ("-o", "--ofile"):
+            outputfile = arg
+            print 'Input file is "', inputfile
+            print 'Output file is "', outputfile
+
+    input = open(inputfile,"r")
+    output = open(outputfile,"w")
+
+
+    #tree_str = input.read().splitlines()
+    #tree_str = input.readlines()
+    #print tree_str
+
+    tree = dendropy.Tree.get(path=inputfile,schema="newick")
+
+    #print(tree.as_string(schema='newick'))
+    #print(tree.as_ascii_plot())
+
+    pdc = tree.phylogenetic_distance_matrix()
+    #print len(pdc.distances())
+    count=0
+    num=0
+    numOfNodes = len(tree.taxon_namespace)
+    output_string=str(numOfNodes)+"\n"
+    for i, t1 in enumerate(tree.taxon_namespace[:]):
+        output_string+=str(t1.label)+" "
+        for t2 in tree.taxon_namespace[:]:
+            output_string+=str(pdc(t1, t2))+" "
+            count+=1
+        output_string+="\n"
+    output.write(output_string)
+
+input.close()
+output.close()
+
+
