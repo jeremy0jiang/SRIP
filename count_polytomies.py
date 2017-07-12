e6fcbb54738f23ef55af169d0e64e61e1f51c774
jeremy0jiang <jeremy0jiang@gmail.com>
2017-07-12 11:25:35 -0700

    add comment count_polytomies

--- count_polytomies.py
+++ count_polytomies.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python
+'''
+Shenghao Jiang 2017
+
+This program counts the number of polytomies in the tree
+'''
+#USAGE: python count_polytomies <polytomy_file>
+
 import dendropy
 import sys, getopt
 
@@ -23,12 +30,7 @@ if __name__ == '__main__':
     input= open(inputfile, "r")
 
     tree_str = input.readlines()[0]
-    #tree_str = "[&R] (((1036:0.0214651885794,1035:0.0354841403334)True:0.00275948649595,688:0.0206453098687)True:-5.00000250009e-07,654:0.0206824287469,(182:0.0696922061759,(380:0.0516417428649,362:0.0547875428611)True:0.00792656640408)True:0.0777423069287)True;"
-    #tree_str ="(a,(4,(5,12,34,45,56),6,7),c,d,(e,f,g),(j,i,o,p,r));"
     tree = dendropy.Tree.get(data=tree_str, schema="newick")
-    print(tree.as_ascii_plot())
-    print (tree.as_string(schema='newick'))
-
 
     num_polytomies = count(tree.seed_node)
     print num_polytomies
